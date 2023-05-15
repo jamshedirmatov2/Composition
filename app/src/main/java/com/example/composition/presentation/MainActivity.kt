4580127a3554a4d7b6a25b3076b0380f27a66854
Jamshed <jamshedirmatov2@gmail.com>
2023-05-15 20:24:20 +0500

    ViewBinding return

--- app/src/main/java/com/example/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composition/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.composition.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.composition.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
